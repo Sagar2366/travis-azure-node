451ec42a1c013acfc0a6d3d2725e059613bebd8b
Sagar2366 <sagarutekar2366@gmail.com>
2018-09-19 09:11:14 +0530

    updated app.js file

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ const http = require('http')
 http.createServer((req,res) => {
     res.write('Hello world, welcome to node.js application !')
 }).listen(3456);
+
+console.log('Application running on port 3456')
